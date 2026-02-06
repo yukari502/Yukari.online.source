425a90a0802871b85f8c1f2dc6bdcd986867d94b
Mona <zhixi221@gmail.com>
2026-02-06 22:53:23 +0800

    fuzzy search capabilities

--- static/js/search.js
+++ static/js/search.js
@@ -5,14 +5,14 @@ var lunrIndex,
 function initLunr() {
     // First retrieve the index file
     fetch("/index.json")
-        .then(function(response) {
+        .then(function (response) {
             return response.json();
         })
-        .then(function(data) {
+        .then(function (data) {
             pagesIndex = data;
             // Set up lunrjs by declaring the fields we use
             // Also provide their boost level for the ranking
-            lunrIndex = lunr(function() {
+            lunrIndex = lunr(function () {
                 this.field("title", { boost: 10 });
                 this.field("tags", { boost: 5 });
                 this.field("categories", { boost: 5 });
@@ -20,7 +20,7 @@ function initLunr() {
                 this.ref("permalink");
 
                 // Integate the data from index.json
-                data.forEach(function(page) {
+                data.forEach(function (page) {
                     this.add(page);
                 }, this);
             });
@@ -28,30 +28,45 @@ function initLunr() {
             // After index is ready, checking if a query is provided in the URL
             const urlParams = new URLSearchParams(window.location.search);
             const query = urlParams.get('q');
-            if(query){
-                 document.getElementById('search-input').value = query;
-                 search(query);
+            if (query) {
+                document.getElementById('search-input').value = query;
+                search(query);
             }
         })
-        .catch(function(err) {
+        .catch(function (err) {
             console.error("Error fetching index.json", err);
         });
 }
 
 function search(query) {
-    if(!query) {
+    if (!query) {
         $results.innerHTML = "";
         return;
     }
-    
-    // Find the item in our index corresponding to the lunr one to have more info
-    // Lunr result: 
-    //  {ref: "/section/page1", score: 0.27253277901960256, matchData: {…}}
-    var results = lunrIndex.search(query).map(function(result) {
-            return pagesIndex.filter(function(page) {
-                return page.permalink === result.ref;
-            })[0];
+
+    // Perform a fuzzy search using Lunr query builder
+    // We search for:
+    // 1. Exact match (High boost)
+    // 2. Prefix match (Medium boost) - acts like wildcard *
+    // 3. Fuzzy match (Low boost, edit distance 1) - handles typos
+    var results = lunrIndex.query(function (q) {
+        var terms = query.split(/\s+/);
+        terms.forEach(function (term) {
+            if (term) {
+                // Exact match
+                q.term(term, { boost: 100 });
+                // Prefix match (wildcard)
+                q.term(term, { wildcard: lunr.Query.wildcard.TRAILING, boost: 10 });
+                // Fuzzy match
+                q.term(term, { editDistance: 1, boost: 1 });
+            }
         });
+    }).map(function (result) {
+        return pagesIndex.filter(function (page) {
+            return page.permalink === result.ref;
+        })[0];
+    });
+
     renderResults(results);
 }
 
@@ -61,7 +76,7 @@ function renderResults(results) {
         return;
     }
 
-    var html = results.map(function(result) {
+    var html = results.map(function (result) {
         return `
         <li style="margin-bottom: 20px;">
             <h3><a href="${result.permalink}">${result.title}</a></h3>
@@ -69,7 +84,7 @@ function renderResults(results) {
         </li>
         `;
     }).join("");
-    
+
     $results.innerHTML = html;
 }
 
@@ -77,7 +92,7 @@ function init() {
     $results = document.getElementById("results-container");
     var $searchInput = document.getElementById("search-input");
 
-    $searchInput.addEventListener("keyup", function(e) {
+    $searchInput.addEventListener("keyup", function (e) {
         search(e.target.value);
     });
 
